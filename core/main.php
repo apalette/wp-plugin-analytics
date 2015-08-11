34c2f57c4cedb1fb0b4320e07e0c258f5aa63e13
apalette <aurelie.palette@gmail.com>
2015-08-11 14:14:28 +0200

    Create

---
+++ core/main.php
@@ -0,0 +1,7 @@
+<?php
+
+// The plugin can't be called directly
+if (! function_exists('add_action')) {
+    exit();
+}
+?>
---
+++ wp-plugin-analytics.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WP Google Analytics
+Plugin URI:  https://github.com/apalette/wp-plugin-analytics
+Description: Simple Plugin to embed Google Analytics code
+Version:     1.0
+Author:      Aurélie Palette
+Author URI:  https://github.com/apalette/
+*/
+
+define('WP_PLUGIN_ANALYICS_CORE', __DIR__.'/core/');
+
+require_once WP_PLUGIN_ANALYICS_CORE.'main.php';
+?>
